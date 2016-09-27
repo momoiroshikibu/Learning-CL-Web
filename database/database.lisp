9c15dd829807c7df0c203ed88ea74358466899b7
momoiroshikibu <momoiroshikibu@gmail.com>
2016-09-28 08:48:07 +0900

    Fix export

--- database/database.lisp
+++ database/database.lisp
@@ -1,28 +1,27 @@
 (in-package :cl-user)
 
-
-
-
-(defpackage com.momoiroshikibu.database
-  (:use :cl
-        :dbi
-        ))
-(in-package :com.momoiroshikibu.database)
-
-
-
-(defvar database (dbi:connect :mysql
-                              :database-name "testdb"
-                              :username "testuser"
-                              :password "password"))
-
 (defun get-current-date-in-format-yyyy-mm-dd ()
   (multiple-value-bind (second minute hour day month year) (get-decoded-time)
     (concatenate 'string (princ-to-string year) "-" (princ-to-string month) "-" (princ-to-string day))))
 
 
+(defpackage com.momoiroshikibu.database
+  (:use :cl
+        :dbi)
+  (:export :select-multi
+           :select-one
+           :insert))
+
+(in-package :com.momoiroshikibu.database)
+
+
+(defvar *database* (dbi:connect :mysql
+                                :database-name "testdb"
+                                :username "testuser"
+                                :password "password"))
+
 (defun select-multi (n)
-  (let* ((query (dbi:prepare database
+  (let* ((query (dbi:prepare *database*
                              "select * from users limit ?"))
          (result (dbi:execute query n)))
     (loop for row = (dbi:fetch result)
@@ -32,7 +31,7 @@
 
 
 (defun select-one (id)
-  (let* ((query (dbi:prepare database
+  (let* ((query (dbi:prepare *database*
                              "select * from users where id = ?"))
          (result (dbi:execute query id))
          (row (dbi:fetch result)))
@@ -42,15 +41,10 @@
 
 
 (defun insert (first-name last-name)
-  (let* ((query (dbi:prepare database
+  (let* ((query (dbi:prepare *database*
                              "insert into users values (null, ?, ?, ?)"))
          (current-date (get-current-date-in-format-yyyy-mm-dd))
          (result (dbi:execute query first-name last-name current-date)))
     (dbi:fetch result)))
 
 
-(export '(database
-          get-current-date-in-format-yyyy-mm-dd
-          select-multi
-          select-one
-          insert))
