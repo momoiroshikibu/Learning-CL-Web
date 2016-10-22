48ebaa3c6b1f23394e8d9677926da2edc82d92f4
momoiroshikibu <momoiroshikibu@gmail.com>
2016-10-22 23:53:59 +0900

    remove unnecessary files

--- database/datetime.lisp
+++
@@ -1,9 +0,0 @@
-(in-package :cl-user)
-(defpackage com.momoiroshikibu.datetime
-  (:use :cl)
-  (:export :get-current-date-in-yyyy-mm-dd-format))
-(in-package :com.momoiroshikibu.datetime)
-
-(defun get-current-date-in-yyyy-mm-dd-format ()
-  (multiple-value-bind (second minute hour day month year) (get-decoded-time)
-    (concatenate 'string (princ-to-string year) "-" (princ-to-string month) "-" (princ-to-string day))))
