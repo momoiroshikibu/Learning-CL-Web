5f705d0b858ea9fc61e39ee76bb1e2ff77466001
momoiroshikibu <momoiroshikibu@gmail.com>
2016-10-23 12:38:28 +0900

    move user logics to controller

--- controllers/user-controller.lisp
+++ controllers/user-controller.lisp
@@ -3,13 +3,45 @@
   (:use :cl)
   (:import-from :com.momoiroshikibu.utils
                 :read-file-into-string)
-  (:export :users-new))
+  (:export :users
+           :users-new
+           :users-by-id))
 (in-package :com.momoiroshikibu.controllers)
 
 (defvar *html* (com.momoiroshikibu.utils:read-file-into-string "views/users-new.html"))
 (print *html*)
 
-(defun users-new (env)
+(defun users-new ()
   `(200
     (:content-type "text/html")
     (,*html*)))
+
+(defun users (count)
+  (defun htmlify-user (user-plist)
+    (let ((user-id (getf user-plist :|id|))
+          (first-name (getf user-plist :|first_name|)))
+      (format nil "<a href=users/~A>~A</a>" user-id first-name)))
+
+  (defun listify-user (user-plist)
+    (format nil "<li>~A</li>" (htmlify-user user-plist)))
+
+  (let ((users (com.momoiroshikibu.database:select-multi count)))
+    `(200
+      (:content-type "text/html")
+      ("<a href='/users/new'>/users/new</a>"
+       ,@(loop for row in users
+            collect (listify-user row))))))
+
+(defun users-by-id (user-id)
+  (let ((user (com.momoiroshikibu.database:select-one user-id)))
+    `(200
+      (:content-type "text/plain")
+      (,(format-user user)))))
+
+
+(defun format-user (user-plist)
+  (format nil "ID: ~A~&FirstName: ~A~&LastName: ~A~&CreatedAt: ~A~&"
+          (getf user-plist :|id|)
+          (getf user-plist :|first_name|)
+          (getf user-plist :|last_name|)
+          (getf user-plist :|created_at|)))
--- server.lisp
+++ server.lisp
@@ -9,25 +9,11 @@
                 :request-cookies))
 (in-package :com.momoiroshikibu.server)
 
-(defun format-user (user-plist)
-  (format nil "ID: ~A~&FirstName: ~A~&LastName: ~A~&CreatedAt: ~A~&"
-          (getf user-plist :|id|)
-          (getf user-plist :|first_name|)
-          (getf user-plist :|last_name|)
-          (getf user-plist :|created_at|)))
-
-(defun htmlify-user (user-plist)
-  (let ((user-id (getf user-plist :|id|))
-        (first-name (getf user-plist :|first_name|)))
-    (format nil "<a href=users/~A>~A</a>" user-id first-name)))
-
-(defun listify-user (user-plist)
-  (format nil "<li>~A</li>" (htmlify-user user-plist)))
 
 (defun routing=user-id (path)
   (ppcre:register-groups-bind (user-id)
-      ("/users/([0-9]+)" path :sharedp t)
-    (list user-id)))
+                              ("/users/([0-9]+)" path :sharedp t)
+                              (list user-id)))
 
 (defun get-request-value (pairs key)
   (defun iter (pairs key)
@@ -44,21 +30,9 @@
 
 (lambda (env)
   (let ((request-path (getf env :path-info)))
-    (cond ((path "/usres" request-path)
-           `(200
-             (:content-type "text/html")
-             ("<html><body>"
-              "<a href='/users'>/users</a>"
-              "<a href='/users/new'>/users/new</a>"
-              "</body></html>")))
-
-          ((path "/users" request-path)
+    (cond ((path "/users" request-path)
            (cond ((string= (getf env :request-method) "GET")
-                  `(200
-                    (:content-type "text/html")
-                    ("<a href='/users/new'>/users/new</a>"
-                     ,@(loop for row in (com.momoiroshikibu.database:select-multi 1000)
-                          collect (listify-user row)))))
+                  (com.momoiroshikibu.controllers:users 1000))
                  ((string= (getf env :request-method) "POST")
                   (let* ((request (lack.request:make-request env))
                          (body-parameters (lack.request:request-body-parameters request)))
@@ -69,18 +43,13 @@
                       (:location "/users"))))))
 
           ((path "/users/new" request-path)
-           (com.momoiroshikibu.controllers:users-new env))
-          ((path "/users" request-path)
-           `(200
-             (:content-type "text/html")
-             ("<h1>create new user</h1>")))
+           (com.momoiroshikibu.controllers:users-new))
 
           ((routing=user-id request-path)
-           `(200
-             (:content-type "text/plain")
-             (,(format-user (com.momoiroshikibu.database:select-one (car (routing=user-id request-path)))))))
+           (let ((user-id (car (routing=user-id request-path))))
+             (com.momoiroshikibu.controllers:users-by-id user-id)))
 
           (t
            '(404
-             (:content-type "text/plain")
-             ("Not Found"))))))
+             (:content-type "text/html")
+             ("<h1>404 Not Found</h1>"))))))
