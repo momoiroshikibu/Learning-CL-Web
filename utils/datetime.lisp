7560e965adfa73b09c9be89d49112417bb4909cf
momoiroshikibu <momoiroshikibu@gmail.com>
2016-10-29 19:48:02 +0900

    declare ignore

--- utils/datetime.lisp
+++ utils/datetime.lisp
@@ -5,5 +5,7 @@
 (in-package :com.momoiroshikibu.datetime)
 
 (defun get-current-date-in-yyyy-mm-dd-format ()
-  (multiple-value-bind (second minute hour day month year) (get-decoded-time)
+  (multiple-value-bind (second minute hour day month year)
+      (get-decoded-time)
+    (declare (ignore second minute hour))
     (concatenate 'string (princ-to-string year) "-" (princ-to-string month) "-" (princ-to-string day))))
