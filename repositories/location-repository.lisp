fe123064026ed5dabb48d6fe63efbf647fa2da16
momoiroshikibu <momoiroshikibu@gmail.com>
2016-11-04 07:53:47 +0900

    with-transaction

--- repositories/location-repository.lisp
+++ repositories/location-repository.lisp
@@ -2,6 +2,9 @@
 (defpackage com.momoiroshikibu.repositories.location
   (:use :cl
         :dbi)
+  (:import-from :dbi
+                :with-transaction
+                :do-sql)
   (:import-from :com.momoiroshikibu.database.connection
                 :*connection*)
   (:import-from :com.momoiroshikibu.datetime
@@ -45,11 +48,17 @@
                    :updated-by (getf row :|updated_by|))))
 
 (defun create-location (user-id lat lng)
+  (with-transaction *connection*
+    (let* ((query (dbi:prepare *connection*
+                               "insert into locations values (null, ?, ?, ?, ?, null, null)"))
+           (current-date (get-current-date-in-yyyy-mm-dd-format))
+           (result (dbi:execute query lat lng current-date user-id)))
+      (dbi:fetch result))))
+
+
+(defun update-location (location)
   (let* ((query (dbi:prepare *connection*
-                             "insert into locations values (null, ?, ?, ?, ?, null, null)"))
-         (current-date (com.momoiroshikibu.datetime:get-current-date-in-yyyy-mm-dd-format))
-         (result (dbi:execute query lat lng current-date user-id)))
-    (dbi:fetch result)))
+                             "update locations set column1 = value1 where id = ?")))))
 
 (defun destroy-location-by-id (id)
   (let ((query (dbi:prepare *connection*
