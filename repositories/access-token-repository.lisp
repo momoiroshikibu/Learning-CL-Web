f599d101b9acad5268d462ebf2c58be53e9617b3
momoiroshikibu <momoiroshikibu@gmail.com>
2016-11-05 23:44:37 +0900

    get-access-tokens

--- repositories/access-token-repository.lisp
+++ repositories/access-token-repository.lisp
@@ -9,8 +9,23 @@
   (:export :get-access-token-by-id))
 (in-package :com.momoiroshikibu.repositories.access-token)
 
+(defvar *get-access-tokens-prepared-statement* (dbi:prepare *connection*
+                                                        "select * from access_tokens limit ?"))
+
 (defvar *get-access-token-by-id-prepared-statement* (dbi:prepare *connection*
                                                                  "select * from access_tokens where id = ?"))
+
+
+(defun get-access-tokens (limit)
+  (let* ((result (dbi:execute *get-access-tokens-prepared-statement* limit)))
+    (loop for row = (dbi:fetch result)
+       while row
+       collect (make-instance 'access-token
+                              :id (getf row :|id|)
+                              :user-id (getf row :|user_id|)
+                              :access-token (getf row :|access_token|)
+                              :created-at (getf row :|created_at|)))))
+
 
 (defun get-access-token-by-id (id)
   (let* ((result (dbi:execute *get-access-token-by-id-prepared-statement* id))
