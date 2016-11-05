0896313403cae4771b527e379078c4cc077a1e72
momoiroshikibu <momoiroshikibu@gmail.com>
2016-11-05 23:04:11 +0900

    remove unnecessary column

--- sql/access_tokens.sql
+++ sql/access_tokens.sql
@@ -2,7 +2,6 @@ create table access_tokens (
     `id` integer auto_increment,
     `user_id` integer,
     `access_token` varchar(255),
-    `last_name` varchar(255),
     `created_at` date,
     primary key(id)
 );
