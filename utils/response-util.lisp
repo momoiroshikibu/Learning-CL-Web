f3295d6d92c8f388d58d414a137cf96751cf4fcd
momoiroshikibu <momoiroshikibu@gmail.com>
2016-11-30 21:29:44 +0900

    201

--- utils/response-util.lisp
+++ utils/response-util.lisp
@@ -1,10 +1,16 @@
 (in-package :cl-user)
 (defpackage com.momoiroshikibu.utils.response-util
   (:use :cl)
-  (:export :200-OK))
+  (:export :200-OK
+           :201-CREATED))
 (in-package :com.momoiroshikibu.utils.response-util)
 
 (defun 200-OK (body)
   `(200
+    (:content-type "application/json")
+    (,body)))
+
+(defun 201-CREATED (body)
+  `(201
     (:content-type "application/json")
     (,body)))
